c1d6bab8d4f28a86732da0b6b09b1348b8f3800d
R3C0D3r <cryptographcode@gmail.com>
2019-07-22 16:48:22 +0900

    Update cube_compiler.py

--- cube_compiler.py
+++ cube_compiler.py
@@ -2,6 +2,7 @@ import numpy as np
 import argparse
 from cube import *
 
+# only fore argument parsing
 def str2bool(v):
     if v.lower() in ["true", "t", "1"]:
         return True
@@ -10,6 +11,8 @@ def str2bool(v):
     else:
         raise argparse.ArgumentTypeError("Not Boolean value")
 
+# program argument
+# https://docs.python.org/ko/3/howto/argparse.html
 parser = argparse.ArgumentParser()
 parser.add_argument("-dl","--debug-level", type=int, choices=[0,1,2], default=1,
         help = "Debug: Change Debug Message Level, 0=Off, 1=Basic, 2=Commands")
@@ -24,6 +27,7 @@ parser.add_argument("-a","--ascii", type=str2bool, default=True,
 
 args = parser.parse_args()
 
+# 혹시 모를 상황을 대비한 변수 재선언
 CONFIG_DEBUG = args.debug_level
 CONFIG_CUBE = args.debug_cube
 CONFIG_STEP = args.debug_step
@@ -32,6 +36,9 @@ CONFIG_ASCII = args.ascii
 
 c_list = list()
 
+import logging
+
+# 디버그 옵션이 켜져 있으면 출력, 아니면 아무것도 안 함
 def printd(s, m = 1, CONFIG_DEBUG = 1):
     if CONFIG_DEBUG == 0:
         return None
@@ -40,19 +47,21 @@ def printd(s, m = 1, CONFIG_DEBUG = 1):
 
 if __name__ == "__main__":
     str_input = ""
-    if CONFIG_INPUT_FILE != "":
+    if CONFIG_INPUT_FILE != "": # 지정된 파일에서 cube 스크립트 실행
         printd("Reading From File: %s"%CONFIG_INPUT_FILE)
         f = open(CONFIG_INPUT_FILE,"r")
         str_input = f.read()
         f.close()
-    else:
+    else: # 아닐 경우, 직접 스크립트를 콘솔창에 입력
         printd("Input your script")
         str_input = input()
-
+    
+    # 스크립트 정보: 길이나 무슨 스크립트가 입력되었는지
     printd("Script Loaded with Length %d"%len(str_input))
     printd(str_input)
-    str_index = 0
-    par_stack = list()
+
+    str_index = 0 # 아마도 필요없는 라인
+    par_stack = list() # 아마도 필요없는 라인
     c = Cubes(CONFIG_DEBUG, CONFIG_ASCII, CONFIG_CUBE, CONFIG_STEP)
     print("*"*50)
     print("%30s"%"Result")
